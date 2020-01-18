001b64fca9529b2e8afeb7b22975eec0702b8173
baimx-001 <1430143371@qq.com>
2020-01-18 21:51:28 +0800

    实现考试功能

--- src/main/java/com/study/exam/controller/ExamController.java
+++ src/main/java/com/study/exam/controller/ExamController.java
@@ -4,6 +4,6 @@ public class ExamController {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("fjlsakdjg");
- System.out.println("fjlsakdjg");
+        System.out.println("fjlsakdjg");
     }
 }
