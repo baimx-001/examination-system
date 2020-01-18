74b6a61b2888ce88cd9578f60abfd590f54f6f0d
baimx-001 <1430143371@qq.com>
2020-01-18 21:45:44 +0800

    第二次提交

--- src/main/java/com/study/exam/controller/ExamController.java
+++ src/main/java/com/study/exam/controller/ExamController.java
@@ -3,5 +3,6 @@ package com.study.exam.controller;
 public class ExamController {
     public static void main(String[] args) {
         System.out.println("111");
+        System.out.println("fjlsakdjg");
     }
 }
