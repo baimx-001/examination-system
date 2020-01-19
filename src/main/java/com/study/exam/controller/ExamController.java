04777f307d4fcb792f27c7bb65121b67651fb60c
baimx-001 <1430143371@qq.com>
2020-01-19 13:57:08 +0800

    实现考试功能

--- src/main/java/com/study/exam/controller/ExamController.java
+++ src/main/java/com/study/exam/controller/ExamController.java
@@ -1,9 +1,24 @@
 package com.study.exam.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ExamController {
-    public static void main(String[] args) {
-        System.out.println("111");
-        System.out.println("fjlsakdjg");
-        System.out.println("fjlsakdjg");
+
+    /**
+     * 测试mvc
+     * @return
+     */
+    @RequestMapping("/testController")
+    @ResponseBody
+   public String testController(){
+       return "baimx";
+   }
+    @RequestMapping("/testHtml")
+    public ModelAndView testHtml(){
+        return new ModelAndView("test1");//WEB-INF/test.html
     }
 }
